f9352afdc928ca9d13168154fe5874bb35d6d490
Oğulcan Özdemir <ogulcan016@gmail.com>
2025-02-14 12:41:02 +0300

    Updated types and images

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,13 +1,25 @@
 "use client";
 
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 
+interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  image: string;
+  location: {
+    name: string;
+    url: string;
+  };
+  episode: string[];
+}
+
 function HomePage() {
-  const [characters, setCharacters] = useState<any[]>([]);
+  const [characters, setCharacters] = useState<Character[]>([]);
   const [page, setPage] = useState<number>(1);
-  const recordsPerPage = 5;
   const [search, setSearch] = useState<string>("");
   const [episodeNames, setEpisodeNames] = useState<{ [key: number]: string }>(
     {}
@@ -19,7 +31,7 @@ function HomePage() {
         `https://rickandmortyapi.com/api/character?page=${page}&name=${search}`,
         { params: { count: 3 } }
       )
-      .then((response: any) => {
+      .then((response) => {
         setCharacters(response.data.results.splice(0, 18));
       });
   }, [page, search]);
@@ -33,7 +45,7 @@ function HomePage() {
       if (character.episode.length > 0) {
         axios
           .get(character.episode[0])
-          .then((response: any) => {
+          .then((response) => {
             setEpisodeNames((prev) => ({
               ...prev,
               [character.id]: response.data.name,
@@ -52,96 +64,122 @@ function HomePage() {
   const endPage = Math.min(totalPages, startPage + maxPageNumbers - 1);
 
   return (
-    <div className="container mx-auto my-8">
-      <div className="flex items-center mb-4">
-        <img
-          src="./favicon.ico"
-          alt="Rick & Morty Logo"
-          className="w-10 mr-4"
-        />
-        <h1 className="text-2xl font-bold">The Rick and Morty App</h1>
-      </div>
-      <div className="flex justify-between my-10">
-        <input
-          type="text"
-          placeholder="Search characters"
-          value={search}
-          onChange={handleSearch}
-          className=" p-2 border border-gray-600s rounded w-1/5"
-        />
-        <nav className="flex mt-4 justify-center">
-          <ul className="flex space-x-2">
-            <li
-              onClick={() => setPage(page > 1 ? page - 1 : 1)}
-              className="p-2 bg-gray-300 rounded cursor-pointer"
-            >
-              Previous
-            </li>
-            {Array.from(
-              { length: endPage - startPage + 1 },
-              (_, i) => startPage + i
-            ).map((pageNumber) => (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-6xl">
+        <div className="flex items-center mb-4">
+          <Image
+            src="/favicon.ico"
+            alt="Rick & Morty Logo"
+            width={40}
+            height={40}
+            className="mr-4"
+          />
+          <h1 className="text-2xl font-bold">The Rick and Morty App</h1>
+        </div>
+        <div className="flex justify-between my-10">
+          <input
+            type="text"
+            placeholder="Search characters"
+            value={search}
+            onChange={handleSearch}
+            className=" p-2 border border-gray-600 rounded w-1/5"
+          />
+          <nav className="flex mt-4 justify-center">
+            <ul className="flex space-x-2">
               <li
-                key={pageNumber}
-                onClick={() => setPage(pageNumber)}
-                className={`px-4 py-2 rounded cursor-pointer ${
-                  pageNumber === page ? "bg-blue-500 text-white" : "bg-gray-300"
-                }`}
+                onClick={() => setPage(page > 1 ? page - 1 : 1)}
+                className="p-2 bg-gray-300 rounded cursor-pointer"
               >
-                {pageNumber}
+                Previous
               </li>
-            ))}
-            <li
-              onClick={() => setPage(page < totalPages ? page + 1 : totalPages)}
-              className="p-2 bg-gray-300 rounded cursor-pointer"
+              {Array.from(
+                { length: endPage - startPage + 1 },
+                (_, i) => startPage + i
+              ).map((pageNumber) => (
+                <li
+                  key={pageNumber}
+                  onClick={() => setPage(pageNumber)}
+                  className={`px-4 py-2 rounded cursor-pointer ${
+                    pageNumber === page
+                      ? "bg-blue-500 text-white"
+                      : "bg-gray-300"
+                  }`}
+                >
+                  {pageNumber}
+                </li>
+              ))}
+              <li
+                onClick={() =>
+                  setPage(page < totalPages ? page + 1 : totalPages)
+                }
+                className="p-2 bg-gray-300 rounded cursor-pointer"
+              >
+                Next
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 bg-gray-800 p-10 rounded-lg">
+          {characters.map((character) => (
+            <div
+              key={character.id}
+              className="flex bg-gray-600 text-white rounded-xl overflow-hidden"
             >
-              Next
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 bg-gray-800 p-10 rounded-lg">
-        {characters.map((character: any) => (
-          <div
-            key={character.id}
-            className="flex bg-gray-600 text-white rounded-xl overflow-hidden"
-          >
-            <Image
-              width={208}
-              height={208}
-              alt={character.name}
-              src={character.image}
-            />
-            <div className="flex flex-col justify-center items-start text-base ml-2">
-              <h2 className="text-2xl font-bold">{character.name}</h2>
-              {character.status === "Alive" ? (
-                <p className="font-semibold flex    ">
-                  <img src="./green.svg" alt="" className="w-3 mr-2" />{" "}
-                  {character.status} - {character.species}
-                </p>
-              ) : character.status === "Dead" ? (
-                <p className="font-semibold flex">
-                  <img src="./red.svg" alt="" className="w-3 mr-2" />{" "}
-                  {character.status} - {character.species}
-                </p>
-              ) : (
-                <p className="font-semibold flex">
-                  <img src="./gray.svg" alt="" className="w-3 mr-2" />{" "}
-                  {character.status} - {character.species}
-                </p>
-              )}
+              <Image
+                width={208}
+                height={208}
+                alt={character.name}
+                src={character.image}
+              />
+              <div className="flex flex-col justify-center items-start text-base ml-2">
+                <h2 className="text-2xl font-bold">{character.name}</h2>
+                {character.status === "Alive" ? (
+                  <p className="font-semibold flex">
+                    <Image
+                      src="/green.svg"
+                      alt=""
+                      width={12}
+                      height={12}
+                      className="mr-2"
+                    />{" "}
+                    {character.status} - {character.species}
+                  </p>
+                ) : character.status === "Dead" ? (
+                  <p className="font-semibold flex">
+                    <Image
+                      src="/red.svg"
+                      alt=""
+                      width={12}
+                      height={12}
+                      className="mr-2"
+                    />{" "}
+                    {character.status} - {character.species}
+                  </p>
+                ) : (
+                  <p className="font-semibold flex">
+                    <Image
+                      src="/gray.svg"
+                      alt=""
+                      width={12}
+                      height={12}
+                      className="mr-2"
+                    />{" "}
+                    {character.status} - {character.species}
+                  </p>
+                )}
 
-              <p className="text-gray-400 mt-2">Last known location:</p>
-              <a href={character.location.url} className="text-lg">
-                {character.location.name}
-              </a>
-              <p className="text-gray-400 mt-2">First seen in:</p>
-              <a href={character.episode[0]} className="text-lg">
-                {episodeNames[character.id] || "Loading..."}
-              </a>
+                <p className="text-gray-400 mt-2">Last known location:</p>
+                <a href={character.location.url} className="text-lg">
+                  {character.location.name}
+                </a>
+                <p className="text-gray-400 mt-2">First seen in:</p>
+                <a href={character.episode[0]} className="text-lg">
+                  {episodeNames[character.id] || "Loading..."}
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
